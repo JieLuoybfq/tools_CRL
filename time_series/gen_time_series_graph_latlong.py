54337298692dc51c2f1df025a3ab03bc5a69ec1c
clevy2013 <58232811+clevy2013@users.noreply.github.com>
2020-02-19 16:19:36 -0500

    Update gen_time_series_graph_latlong.py

--- time_series/gen_time_series_graph_latlong.py
+++ time_series/gen_time_series_graph_latlong.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 years = ["2019"]
 #tile = "h12v04"
 prdct = "MCD43A3"
-base_dir = '/muddy/data04/charlotte.levy/outputs/LANCE_out'
+base_dir = '/muddy/data04/charlotte.levy/outputs/LANCE_out/2019_h12v04'
 
 sites_dict = {
 "HF" : [(42.53691, -72.17265), "h12v04"]}
