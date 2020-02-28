23ff5e0b02e48160473c386b4beb590163a58a47
clevy2013 <58232811+clevy2013@users.noreply.github.com>
2020-02-28 16:22:57 -0500

    Updated Band name

--- hdf_to_tif_mcd43.sh
+++ hdf_to_tif_mcd43.sh
@@ -7,17 +7,17 @@ if [ ! -d ${out_dir} ]; then
     mkdir ${out_dir}
 fi
 
-if [ ! -d ${out_dir}/qa/ ]; then
-    mkdir ${out_dir}/qa/
-fi
+#if [ ! -d ${out_dir}/qa/ ]; then
+#    mkdir ${out_dir}/qa/
+#fi
 
-if [ ! -d ${out_dir}/wsa/ ]; then
-    mkdir ${out_dir}/wsa/
-fi
+#if [ ! -d ${out_dir}/wsa/ ]; then
+#    mkdir ${out_dir}/wsa/
+#fi
 
-if [ ! -d ${out_dir}/bsa/ ]; then
-    mkdir ${out_dir}/bsa/
-fi
+#if [ ! -d ${out_dir}/bsa/ ]; then
+#    mkdir ${out_dir}/bsa/
+#fi
 
 for hdf in $in_dir/*.hdf
 do
@@ -25,13 +25,13 @@ do
     filename=$(basename -- $hdf)
     extension="${filename##*.}"
     filename_bare="${filename%.*}"
-    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b01_c ${out_dir}/${filename_bare}_sr_b1.tif
-    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b02_c ${out_dir}/${filename_bare}_sr_b2.tif
-    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b03_c ${out_dir}/${filename_bare}_sr_b3.tif
-    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b04_c ${out_dir}/${filename_bare}_sr_b4.tif
-    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b05_c ${out_dir}/${filename_bare}_sr_b5.tif
-    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b06_c ${out_dir}/${filename_bare}_sr_b6.tif
-    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b07_c ${out_dir}/${filename_bare}_sr_b7.tif
+    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b01_1 ${out_dir}/${filename_bare}_sr_b1.tif
+    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b02_1 ${out_dir}/${filename_bare}_sr_b2.tif
+    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b03_1 ${out_dir}/${filename_bare}_sr_b3.tif
+    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b04_1 ${out_dir}/${filename_bare}_sr_b4.tif
+    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b05_1 ${out_dir}/${filename_bare}_sr_b5.tif
+    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b06_1 ${out_dir}/${filename_bare}_sr_b6.tif
+    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b07_1 ${out_dir}/${filename_bare}_sr_b7.tif
 
     #gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:Albedo_WSA_Band1 ${out_dir}/${filename_bare}_wsa_b1.tif
     #gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:Albedo_WSA_Band2 ${out_dir}/${filename_bare}_wsa_b2.tif
