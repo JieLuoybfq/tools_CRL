e265a4055e6ed5892ea82bcaabbe7a4e28a7d2b1
clevy2013 <58232811+clevy2013@users.noreply.github.com>
2020-03-13 00:24:58 -0400

    Change Base Directory

--- gen_time_series_graph_latlong.py
+++ gen_time_series_graph_latlong.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 years = ["2019"]
 tile = "h12v04"
 prdct = "MCD43A3"
-base_dir = '/muddy/data04/charlotte.levy/outputs/Shift/Zero'
+base_dir = '/muddy/data04/charlotte.levy/outputs/Shift/Reverse'
 
 sites_dict = {
 "HarvardForest" : [(42.53691, -72.17265), tile],
