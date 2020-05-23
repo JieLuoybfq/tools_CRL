7ad9e6c4f52bf2dcd38efc1df6d81002838475e0
Charlotte Levy <charlotte.levy@umb.edu>
2020-05-23 11:40:15 -0400

    updated for loop: mass download and test now working

--- download_viirs_modis_2climoOpt.sh
+++ download_viirs_modis_2climoOpt.sh
@@ -7,7 +7,7 @@
 # This will soon change. I don't think it is worth adding an argument for this; just change
 # it when the collection 061 and 002 processing (et cetera) are done.
 
-usage="Usage: ./download_modis.sh [-s start date (YYYY-MM-DD)] [-e end date (YYYY-MM-DD)] [-n product short name e.g. MCD43A3] [-t tile e.g. h12v04] [-d download dir] [-m]"
+usage="Usage: ./download_modis.sh [-s start date (YYYY-MM-DD)] [-e end date (YYYY-MM-DD)] [-n product short name e.g. MCD43A3] [-t tile e.g. h12v04] [-d download dir] [-m file location]"
 while getopts ":s:e:n:t:d:m:" arg; do
     case $arg in
 	s) start_date=$OPTARG;;
@@ -15,7 +15,7 @@ while getopts ":s:e:n:t:d:m:" arg; do
 	n) short_name=$OPTARG;;
 	t) tile=$OPTARG;;
 	d) dl_dir=$OPTARG;;
-	m) multiple="TRUE";;
+	m) multiple=$OPTARG;;
 	\?) echo $usage
     esac
 done
@@ -30,13 +30,14 @@ else
     echo "All args ok"
 fi
 
-#iCreate alternate tile configuration for loop.
-#TO DO take input of a file instead 
-if [ -z $multiple]; then
+#Create alternate tile configuration for loop.
+echo $multiple
+if [ -z $multiple ]; then
     echo "Single Tile Option"
+    tile_list=$tile
 else
     echo "Multiple Tile Option"
-    tile_list="LIST"
+    tile_list="h01v09 h01v10"
 fi
 
 # Check if the download dir has a trailing slash, add if not
@@ -66,32 +67,33 @@ case ${short_name} in
 	    fmt="h5";;
 esac
 
-cur_date=${start_date}
-end_date=$(date -I -d "$end_date+1 day")
-
+#cur_date=${start_date}
+#last_date=$(date -I -d "$end_date day")
 
 #Begin loop for tile list, if applicable
 for newtile in $tile_list; do
     tile=$newtile
-    echo "Now Beginning " $newtile
+    echo "Now Beginning " ${newtile}
 # Loop through all dates in year, download with full url
-while [[ "$cur_date" < "$end_date" ]]; do 
-    dl_dir_out=$dl_dir
-    year=`date --date="$cur_date" '+%Y'`
-    cur_date_url=${cur_date:0:4}.${cur_date:5:2}.${cur_date:8:2}
-    dl_url=${url_prod}${cur_date_url}/
-    file="${short_name}*${tile}*.${fmt}"
-    cur_date=$(date -I -d "$cur_date+1 day")
-   # dl_dir_out=${dl_dir_out}${short_name}/${fmt}/${year}/${tile}
-    dl_dir_out=${dl_dir_out}${tile}/${short_name}/${year}
-    if [ ! -r $dl_dir_out ]; then
-	mkdir -p $dl_dir_out
-    fi
-    echo "Downloading from:" ${dl_url}
-    echo "File: " ${file}
-    echo "Saving to: " ${dl_dir_out}
-    wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies --no-check-certificate --auth-no-challenge=on \
+    cur_date=${start_date}
+    last_date=$(date -I -d "$end_date day")
+    while [[ "$cur_date" < "$last_date" ]]; do 
+       dl_dir_out=$dl_dir
+       year=`date --date="$cur_date" '+%Y'`
+       cur_date_url=${cur_date:0:4}.${cur_date:5:2}.${cur_date:8:2}
+       dl_url=${url_prod}${cur_date_url}/
+       file="${short_name}*${tile}*.${fmt}"
+       cur_date=$(date -I -d "$cur_date+1 day")
+      #dl_dir_out=${dl_dir_out}${short_name}/${fmt}/${year}/${tile}
+       dl_dir_out=${dl_dir_out}${tile}/${short_name}/${year}
+       if [ ! -r $dl_dir_out ]; then
+	   mkdir -p $dl_dir_out
+       fi
+       echo "Downloading from: " ${dl_url}
+       echo "File: " ${file}
+       echo "Saving to: " ${dl_dir_out}
+       wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies --no-check-certificate --auth-no-challenge=on \
 	 -r --reject "index.html*" --accept "${file}" -l1 -np -e robots=off --no-directories --waitretry=300 -t 100 \
 	 --directory-prefix=${dl_dir_out} --secure-protocol=TLSv1 ${dl_url} --limit-rate=350M
-done
+    done
 done
